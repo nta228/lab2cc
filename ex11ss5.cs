085d22536f16762aa9ae0d77ed4f99599b524b20
nta228 <64982003+nta228@users.noreply.github.com>
2021-01-06 06:21:08 +0700

    ss5

---
+++ ex11ss5.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeSnipet11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] studentNames = new string[3] { "Ashley", "Joe", "Mikel"};
+            foreach (string studName in studentNames)
+            {
+                {
+                    Console.WriteLine("Congratulations!! " +studName + " you have beengranted an extra leave");
+                }
+            }
+        }
+    }
+}
---
+++ ex6ss5.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeSnipet6
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int j;
+            int i;
+            int[,] dimension = new int[4,5];
+            int numOne = 0;
+            for ( i = 0; i < 4; i++)
+            {
+                for ( j = 0; j < 5; j++)
+                {
+                    dimension[i,j] = numOne;
+                    numOne++;
+                }
+            }
+            for ( i = 0; i < 4; i++)
+            {
+                for ( j = 0; j < 5; j++)
+                {
+                    Console.Write(dimension[i,j] + "");
+                }
+                Console.Write(dimension[i,j] + "");
+            }
+            Console.WriteLine();
+        }
+    }
+ }
+
